1d8fb3f0b52145469839b627f9a23eeb0f4d156c
nrivas-itba <138261486+nrivas-itba@users.noreply.github.com>
2025-04-13 17:16:32 -0300

    now player thinks without taking the semaphore

--- src/player10/player10.c
+++ src/player10/player10.c
@@ -5,12 +5,13 @@
 #include <stdint.h>
 #include <unistd.h>
 #include "../utils/utils.h"
+#include <string.h> //for mem cpy
 
-player_t* getMe(game_t* game){
+unsigned int getMe(game_t* game){
     pid_t pid = getpid();
     for(int i =0; i<MAX_PLAYERS; i++){
         if(game->state->playerList[i].pid == pid){
-            return &(game->state->playerList[i]);
+            return i;
         }
     }
     errExit("I dont exist in the board");
@@ -27,52 +28,72 @@ char shouldITryToMove(player_t* me){
     return 0;
 }
 
-int main(int argc, char* argv[]){
-    game_t game = openGame(argc, argv);
-    player_t* me = getMe(&game);
+void waitToRead(gameSync_t* gameSync){
+    sWait(&(gameSync->masterWantsToReadMutex));
+    sPost(&(gameSync->masterWantsToReadMutex)); //In the 31/3/2025 we saw that it should have this order
 
+    sWait(&(gameSync->readersCountMutex));
+    gameSync->readersCount++;
+    if(gameSync->readersCount == 1){
+        sWait(&(gameSync->readGameStateMutex)); //First reader locks writes
+    }
+    sPost(&(gameSync->readersCountMutex));
+}
+
+void finishReading(gameSync_t* gameSync){
+    sWait(&(gameSync->readersCountMutex));
+    gameSync->readersCount--;
+    if(gameSync->readersCount == 0){
+        sPost(&(gameSync->readGameStateMutex)); //Last readers unlocks writes
+    }
+    sPost(&(gameSync->readersCountMutex));
+}
+
+char getNextMove(){
     FILE *fp = fopen("/dev/random", "rb");
     if (fp == NULL) {
         errExit("Error opening /dev/random");
     }
-
-
     unsigned char random_byte;
-    char isGameOver = 0;
-    while(!isGameOver) {
-        
-        /////////////Readers entrance
-        sWait(&(game.sync->masterWantsToReadMutex));
-        sPost(&(game.sync->masterWantsToReadMutex)); //In the 31/3/2025 we saw that it should have this order
-
-        sWait(&(game.sync->readersCountMutex));
-        game.sync->readersCount++;
-        if(game.sync->readersCount == 1){
-            sWait(&(game.sync->readGameStateMutex)); //First reader locks writes
-        }
-        
-        sPost(&(game.sync->readersCountMutex));
-        /////////////Readers entrance
-
-        /////////////Read //TODO WE SHOULD NOT THINK HERE. Just copy the relevant information and free the sem
-        if(shouldITryToMove(me)){
-            fread(&random_byte, 1, 1, fp);
-            printf("%c", random_byte%8); // Print a random value
-            fflush(stdout);
-        }
-        isGameOver = game.state->isOver;
-
-        /////////////Readers exit
-        sWait(&(game.sync->readersCountMutex));
-        game.sync->readersCount--;
-        if(game.sync->readersCount == 0){
-            sPost(&(game.sync->readGameStateMutex)); //Last readers unlocks writes
-        }
-        sPost(&(game.sync->readersCountMutex));
-        /////////////Readers exit
-    }
-
-    printf("\n");
+    fread(&random_byte, 1, 1, fp);
     fclose(fp);
-    return EXIT_SUCCESS;
+    return random_byte;
+}
+
+void playingLoop(game_t* game, int gameWidth, int gameHeight, unsigned int playerCount, unsigned int me){
+    player_t playerList[playerCount];
+    int board[gameWidth][gameHeight];
+    char isGameOver = 0;
+
+    while(!isGameOver) {
+        memcpy(playerList, game->state->playerList, sizeof(playerList[0])*playerCount);
+        memcpy(board,      game->state->board,      sizeof(board[0][0])*gameWidth*gameHeight);
+        isGameOver = game->state->isOver;
+        finishReading(game->sync);
+
+        if(shouldITryToMove(&(playerList[me]))){
+            char nextMove = getNextMove()%9;
+            if(nextMove!=8){
+                printf("%c", nextMove); // Print a random value
+                fflush(stdout);
+            }
+        }
+        
+        if(!isGameOver){
+            waitToRead(game->sync);
+        }
+        else{
+            break;
+        }
+    }
+}
+
+int main(int argc, char* argv[]){
+    game_t game = openGame(argc, argv); //TODO meter todas estas inicializaciones en un inicializador de view
+    unsigned int me = getMe(&game);
+    
+    waitToRead(game.sync);
+    playingLoop(&game, game.gameWidth, game.gameHeight, game.state->playerCount, me);
+    
+    return 0;
 }
